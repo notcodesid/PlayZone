7cae52d55b036102d1450820b5b2e6f59d0b3c06
siddharth <notcodesid@gmail.com>
2024-11-26 19:38:38 +0530

    minor changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,7 +15,6 @@ function App() {
     <div className="min-h-screen bg-gray-50">
       <Navbar />
       <Hero />
-      <FeaturedProducts />
       <Categories />
       <SpecialOffers />
       <NewArrivals />
